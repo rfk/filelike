2a8d78c9054f3596e838db17f6bed48adb9b0ad8
Ryan Kelly <ryan@rfk.id.au>
2010-01-19 13:15:11 +1100

    bump version number to v0.3.5p1

--- filelike/__init__.py
+++ filelike/__init__.py
@@ -91,7 +91,7 @@ file-like objects:
 __ver_major__ = 0
 __ver_minor__ = 3
 __ver_patch__ = 5
-__ver_sub__ = ""
+__ver_sub__ = "p1"
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
 
 
